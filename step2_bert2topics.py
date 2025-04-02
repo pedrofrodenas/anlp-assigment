a810eaa4b719cdd00ff510e8df7e0b124bcfe84d
JunkyByte <adriano.donninelli@hotmail.it>
2025-04-02 15:56:38 +0200

    moving to article topic directly

--- step2_bert2topics.py
+++ step2_bert2topics.py
@@ -1,6 +1,7 @@
-from transformers import AutoTokenizer, AutoModel
 import torch
-from torch.nn.functional import cosine_similarity
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
 
 
 DEVICE = 'cpu'
@@ -12,72 +13,78 @@ elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
 
 class BertScorer:
     def __init__(self, topics, subtopics):  # TODO: Topics type?
-        model_name = 'dccuchile/bert-base-spanish-wwm-cased'  # BETO cased model
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.model = AutoModel.from_pretrained(model_name, device_map=DEVICE)
+        model_name = "mrm8488/modernbert-embed-base-ft-sts-spanish-matryoshka-768-64-5e"
+        self.model = SentenceTransformer(model_name, device=DEVICE)
 
         # TODO: Decide how relates with subtopics
-        self.topics_embeddings = {topic: self.get_embedding(f'{topic}: {desc}')
-                                  for topic, desc in topics.items()}
+        embeddings = self.model.encode([f'{topic} {desc}' for topic, desc in topics.items()])
+        self.topics_embeddings = {topic: emb for topic, emb in zip(topics.keys(), embeddings)}
 
         self.subtopics_embeddings = {}
         for topic in topics.keys():
-            print(subtopics[topic])
-            self.subtopics_embeddings[topic] = {topic: self.get_embedding(f'{topic}: {desc}')
-                                                for topic, desc in subtopics[topic].items()}
+            embeddings = self.model.encode([f'{topic} {desc}' for topic, desc in subtopics[topic].items()])
+            self.subtopics_embeddings[topic] = {topic: emb for topic, emb in zip(subtopics[topic].keys(), embeddings)}
 
-    # NOTE: Currently NOT in batches
-    def get_embedding(self, text):
-        inputs = self.tokenizer(text, return_tensors='pt', padding=True, truncation=True, max_length=512)
-        assert inputs['input_ids'].shape[0] == 1, 'Batch inference not supported right now'
-
-        with torch.no_grad():
-            outputs = self.model(**inputs)
-        
-        # TODO MAYBE NORMALIZE BERT EMBEDDINGS?
-
-        # Mean pooling to similarity
-        # To do so evaluate padding length and mask it
-        embeddings = outputs.last_hidden_state
-        mask = inputs['attention_mask'].unsqueeze(-1).expand(embeddings.size()).float()
-        # print(mask)
-        masked_embeddings = embeddings * mask
-        summed = torch.sum(masked_embeddings, 1)
-        summed_mask = torch.clamp(mask.sum(1), min=1e-9)
-        emb = summed / summed_mask
-        return emb
-
-    # Currently using mean pooling for scoring
-    # TODO: MAKE IT BATCHED
-    def get_topics_score(self, query):  # TODO: Max length? check beto context window
-        query_emb = self.get_embedding(query)
+    def get_topics_score(self, query: str):
+        query_emb = self.model.encode(query)
         similarities = self._get_scores(query_emb, self.topics_embeddings)
         return similarities
     
-    def _get_scores(self, query_emb, topics_embs):
-        return {topic: cosine_similarity(query_emb, emb).item() for topic, emb in topics_embs.items()}
+    def _get_scores(self, query_emb: np.ndarray, topics_embs: dict):
+        similarities = self.model.similarity(query_emb, np.vstack(list(topics_embs.values())))[0]
+        return {topic: similarity.item() for topic, similarity in zip(topics_embs.keys(), similarities)}
     
-    def get_subtopic_score(self, topic, query):
+    def get_subtopic_score(self, topic: str, query: str):
         assert topic in self.subtopics_embeddings, f'No subtopics defined for topic {topic}'
-        query_emb = self.get_embedding(query)
+        query_emb = self.model.encode(query)
         similarities = self._get_scores(query_emb, self.subtopics_embeddings[topic])
         return similarities
 
 
 if __name__ == '__main__':
     topics = {
-        "REQUISITOS": "Criterios de admisión y elegibilidad",
-        "BECAS": "Información sobre becas y ayudas financieras"
+        "Información General y Tipos de Beca": "",
+        "Estudios Cubiertos por la Beca": "",
+        "Requisitos para Solicitar la Beca": "",
+        "Proceso de Solicitud y Tramitación": "",
+        "Obligaciones, Control y Situaciones Especiales": "",
     }
 
     subtopics = {
-        'REQUISITOS': {
-            'Requisitos economicos': "",  # TODO
-            'Requisitos academicos': "",
+        "Información General y Tipos de Beca": {
+            "Objeto de la Convocatoria y Financiación": "",
+            "Tipos de Cuantías": "",
+            "Beca de Matrícula": "",
+            "Beca Básica": "",
+            "Cuantías Adicionales": "",
         },
 
-        'BECAS': {
-            'Verificación de las becas': "",
+        "Estudios Cubiertos por la Beca": {
+            "Enseñanzas No Universitarias": "",
+            "Enseñanzas Universitarias": "",
+        },
+
+        "Requisitos para Solicitar la Beca": {
+            "Requisitos Generales": "",
+            "Requisitos Económicos": "",
+            "Requisitos Académicos": "",
+        },
+
+        "Proceso de Solicitud y Tramitación": {
+            "Presentación de Solicitud": "",
+            "Documentación y Autorizaciones": "",
+            "Revisión, Subsanación y Alegaciones": "",
+            "Órganos de Selección y Tramitación": "",
+            "Resolución, Notificación y Consulta de Estado": "",
+            "Pago de la Beca": "",
+        },
+
+        "Obligaciones, Control y Situaciones Especiales": {
+            "Obligaciones de los Becarios": "",
+            "Control, Verificación y Reintegro": "",
+            "Compatibilidades e Incompatibilidades con otras ayudas": "",
+            "Situaciones Específicas": "",
+            "Recursos contra la resolución": "",
         }
     }
 
@@ -87,10 +94,9 @@ if __name__ == '__main__':
 
     scorer = BertScorer(topics, subtopics)
 
-    # Compute embeddings
+    # Compute embeddings:
     topics_scores = scorer.get_topics_score(query_chapter)
 
-    # Output similarity scores
     for topic, score in topics_scores.items():
         print(f"Similarity with {topic}: {score:.4f}")
 
@@ -98,7 +104,10 @@ if __name__ == '__main__':
     best_topic = max(topics_scores, key=topics_scores.get)
     print(f"\nThe query '{query_chapter}' is most similar to the topic: '{best_topic}'")
 
-    subtopics_scores = scorer.get_subtopic_score(topic, query_article)
+    subtopics_scores = scorer.get_subtopic_score(best_topic, query_article)
+
+    for subtopic, score in subtopics_scores.items():
+        print(f"Similarity with {subtopic}: {score:.4f}")
 
     # Determine the best matching subtopic
     best_subtopic = max(subtopics_scores, key=subtopics_scores.get)
